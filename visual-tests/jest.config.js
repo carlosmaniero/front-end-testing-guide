6b210ffca457b94cc68be537bc69572956c645f1
Maniero <carlosmaniero@gmail.com>
2021-08-20 00:55:00 -0300

    increases timeout

--- visual-tests/jest.config.js
+++ visual-tests/jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ["<rootDir>jest.setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>jest.setup.ts'],
+  testTimeout: 30000,
 };
