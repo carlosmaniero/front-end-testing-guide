9a4c1e58446fd2404f6fe5c91fed26fc6aa88a41
Maniero <carlosmaniero@gmail.com>
2021-08-20 04:27:42 -0300

    renders pokemon card

--- visual-tests/tests/design-system/card/Card.spec.ts
+++ visual-tests/tests/design-system/card/Card.spec.ts
@@ -2,7 +2,12 @@ import { componentToMatchSnapshot, goToComponent } from '../../config/storybook'
 
 describe('Card', () => {
   it('renders the card with a children', async () => {
-    await goToComponent('design-system-card-card--default-view', async (page) => {
+    await goToComponent('design-system-card-card--default-view', async (page, document) => {
+      await componentToMatchSnapshot(page);
+
+      const component = await document.getByText('Hello, World!');
+      await component.hover();
+
       await componentToMatchSnapshot(page);
     });
   });
